0c7aa21ab2c26cc4a4848b39feb54aa7c03f8b35
Valentine Lejeune <valentine.lejeune1@gmail.com>
2023-03-16 13:39:44 +0100

    style: css footer

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,9 +6,9 @@ import { Toaster } from 'sonner'
 const conmfortaa = Comfortaa({ subsets: ['latin'] })
 
 export default function MyApp({ Component, pageProps }: any) {
-    return <main className={`${conmfortaa.className} h-full grid grid-rows-1`}>
+    return <main className={`${conmfortaa.className} h-full grid grid-rows-[1fr,30px]`}>
         <Component {...pageProps} />
+        <div className='self-center'>Made with 💖 by <a href='https://github.com/Xalisse' className='!cursor-pointer'>Valentine</a></div>
         <Toaster />
-        <div>Made with 💖 by <a href='https://github.com/Xalisse' className='!cursor-pointer'>Valentine</a></div>
     </main>
 }
