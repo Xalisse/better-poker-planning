1300f5768eed052dc21ef23bc6eebfa3596646af
Corentin Chevalier <pro.chevalier.cor@gmail.com>
2023-03-21 12:08:36 +0100

    fix: fixed prettier

--- next.config.js
+++ next.config.js
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    appDir: false,
-  },
+    experimental: {
+        appDir: false,
+    },
 }
 
 module.exports = nextConfig
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,39 +1,44 @@
-import Head from "next/head";
-import { useRouter } from "next/router";
-import { useState } from "react";
-import { v4 as uuidv4 } from "uuid";
+import Head from 'next/head'
+import { useRouter } from 'next/router'
+import { useState } from 'react'
+import { v4 as uuidv4 } from 'uuid'
 
 export default function Home() {
-  const router = useRouter();
-  const [roomName, setRoomName] = useState<string>();
-  const handleNewRoom = (event: any) => {
-    event.preventDefault();
-    const idRoom = uuidv4();
-    localStorage.setItem("currentRoom", JSON.stringify({ idRoom, roomName }));
-    router.push(`/room/${idRoom}?routeName=${roomName?.replaceAll(" ", "-")}`);
-  };
+    const router = useRouter()
+    const [roomName, setRoomName] = useState<string>()
+    const handleNewRoom = (event: any) => {
+        event.preventDefault()
+        const idRoom = uuidv4()
+        localStorage.setItem(
+            'currentRoom',
+            JSON.stringify({ idRoom, roomName })
+        )
+        router.push(
+            `/room/${idRoom}?routeName=${roomName?.replaceAll(' ', '-')}`
+        )
+    }
 
-  return (
-    <>
-      <Head>
-        <title>Better Poker Planning 🦄</title>
-      </Head>
-      <div className="pt-6">
-        <h1 className="mb-44">Better Poker planning</h1>
-        <form
-          onSubmit={handleNewRoom}
-          className="flex flex-col items-center gap-4"
-        >
-          <input
-            name="name"
-            onChange={(e) => setRoomName(e.target.value)}
-            placeholder="Nom de la salle"
-          ></input>
-          <button type="submit" disabled={!roomName}>
-            jouer
-          </button>
-        </form>
-      </div>
-    </>
-  );
+    return (
+        <>
+            <Head>
+                <title>Better Poker Planning 🦄</title>
+            </Head>
+            <div className='pt-6'>
+                <h1 className='mb-44'>Better Poker planning</h1>
+                <form
+                    onSubmit={handleNewRoom}
+                    className='flex flex-col items-center gap-4'
+                >
+                    <input
+                        name='name'
+                        onChange={(e) => setRoomName(e.target.value)}
+                        placeholder='Nom de la salle'
+                    ></input>
+                    <button type='submit' disabled={!roomName}>
+                        jouer
+                    </button>
+                </form>
+            </div>
+        </>
+    )
 }
