3f6b7cbeab165bede9b55ea17f509b77fb1dcef2
Valentine Lejeune <36406706+Xalisse@users.noreply.github.com>
2023-03-22 11:25:00 +0100

    feat: can be a spectator
    * fix: can be a spectator
    
    * refactor: prettier
    
    * fix: can flip card anytime

--- components/UserCard.tsx
+++ components/UserCard.tsx
@@ -21,16 +21,25 @@ const EmptyCard = () => {
     )
 }
 
+const SpectatorCard = () => {
+    return (
+        <div className='flex items-center justify-center rounded-lg w-12 h-20 border-2 border-tertiary border-dashed'>
+            👀
+        </div>
+    )
+}
+
 export default function UserCard({
     user,
     cardValue,
     isFlipped = false,
 }: Props) {
     return (
-        <div className='flex flex-col items-center w-fit'>
-            {isFlipped && <Card value={cardValue || ''} />}
-            {!isFlipped && cardValue && <BackCard />}
-            {!isFlipped && !cardValue && <EmptyCard />}
+        <div className='flex flex-col w-fit m-auto'>
+            {user.isSpectator && <SpectatorCard />}
+            {!user.isSpectator && isFlipped && <Card value={cardValue || ''} />}
+            {!user.isSpectator && !isFlipped && cardValue && <BackCard />}
+            {!user.isSpectator && !isFlipped && !cardValue && <EmptyCard />}
             <p>{user.name}</p>
         </div>
     )
