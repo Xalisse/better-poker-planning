719e093adc996ec7a883c6ca916efe461e61847c
Valentine Lejeune <36406706+Xalisse@users.noreply.github.com>
2023-04-19 14:39:59 +0200

    feat: description should be displayed as html

--- components/StoryDetails.tsx
+++ components/StoryDetails.tsx
@@ -1,21 +1,21 @@
 interface Props {
-    story: {
-        title: string
-        description: string
-        id: string
-        estimation: string
-    }
+  story: {
+    title: string;
+    description: string;
+    id: string;
+    estimation: string;
+  };
 }
 
 const StoryDetails = ({ story }: Props) => {
-    return (
-        <div>
-            <p className='text-lg font-bold'>{story?.title}</p>
-            {story?.estimation && <p>{story.estimation}</p>}
-            {!story?.estimation && <p>Non estimée</p>}
-            <div>{story?.description}</div>
-        </div>
-    )
-}
+  return (
+    <div>
+      <p className="text-lg font-bold">{story?.title}</p>
+      {story?.estimation && <p>{story.estimation}</p>}
+      {!story?.estimation && <p>Non estimée</p>}
+      <div innerHTML={story?.description}></div>
+    </div>
+  );
+};
 
-export default StoryDetails
+export default StoryDetails;
